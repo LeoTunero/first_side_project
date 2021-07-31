dc42af29da30aeaf87d989eb47f4d4b642a8c1cc
Leo <leo.tunero@gmail.com>
2021-07-31 23:04:55 +0800

    add the decorator into the admin.py

--- post/admin.py
+++ post/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
+
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content')
     search_fields = ('title', 'content')
-
-admin.site.register(Post, PostAdmin)
