670f5d3bc045bdb34d05be8b95bbb92cfcacce15
Leo <leo.tunero@gmail.com>
2021-08-11 21:12:33 +0800

    test: try my logic to run the test

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from post.models import Post
 
 # Create your tests here.
+
+
+class PostTestCase (TestCase):
+    def setUp(self) -> None:
+        Post.Meta.verbose_name = 'Essay'
+        Post.Meta.verbose_name_plural = 'Essay'
+        # return super().setUp()
+
+    def test_post_title(self):
+        # Test for practices only
+        'Essay' = Post.Meta.verbose_name.capitalize
+        'Essay' = Post.Meta.verbose_name_plural.capitalize
+        print('test is passed')
