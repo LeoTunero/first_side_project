c31d7d8d55a0f4c604ef85ec1fcc92aea6d1f25e
Leo <leo.tunero@gmail.com>
2021-08-30 21:27:31 +0800

    feat: add the CarViewSetin views.py

--- sharky/car/views.py
+++ sharky/car/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from car.models import Car
+from car.serializers import CarSerializer
+
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
